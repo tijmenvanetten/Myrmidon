5e245203c11e68176f3427155a7c7f5a871c45f4
tijmenvanetten <33256845+tijmenvanetten@users.noreply.github.com>
2021-07-16 01:28:16 +0200

    Integrate API messaging

--- MyrmidonBot/actions/actions.py
+++ MyrmidonBot/actions/actions.py
@@ -14,6 +14,7 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 
 import requests
+import json
 
 
 class RequestDiagnosis(Action):
@@ -25,9 +26,14 @@ class RequestDiagnosis(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        url = 'http://127.0.0.1:5006'
-        response = requests.get(url).text
+        context = {  
+            "sender": "test_user",  
+            "message": tracker.current_state()['latest_message'],
+        }
 
+        url = 'http://127.0.0.1:5006/webhooks/rest/webhook'
+
+        response = requests.post(url, data=json.dumps(context)).text
         response = "DiagnosisBot: " + response
 
         dispatcher.utter_message(response = "give_diagnosis", text = response)
@@ -43,9 +49,14 @@ class ScheduleAppointment(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        url = 'http://127.0.0.1:5007'
-        response = requests.get(url).text
+        url = 'http://127.0.0.1:5007/webhooks/rest/webhook'
 
+        context = {  
+            "sender": "test_user",  
+            "message": tracker.current_state()['latest_message']
+        }
+
+        response = requests.post(url, data=json.dumps(context)).text
         response = "AppointmentBot: " + response
 
         dispatcher.utter_message(response = "give_appointment", text = response)
