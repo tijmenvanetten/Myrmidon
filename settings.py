783318a59dbc32663b4352c81117c3d941ef4b0f
tijmenvanetten <33256845+tijmenvanetten@users.noreply.github.com>
2021-07-15 22:02:22 +0200

    Add settings

--- settings.py
+++ settings.py
@@ -6,14 +6,25 @@ from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
+print(BASE_DIR)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # Application definition
 
-INSTALLED_BOTS = [
+LOCAL_BOTS = [
+    # Local bots
     'appointment_bot',
     'diagnose_bot',
+]
+
+EXTERNAL_BOTS = [
+    {
+        'host': '127.0.0.1', 
+        'port': '5000', 
+        'username': 'admin', 
+        'password': 'admin'
+    }
 ]
