e3ff5479d35b4dfdc3be53127751802f28d1497e
PhilipG <philippos.g@me.com>
2021-07-16 14:24:08 +0300

    Slots now work properly.

--- external/AppointmentBot/actions/actions.py
+++ external/AppointmentBot/actions/actions.py
@@ -8,12 +8,12 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
-#
+from rasa_sdk.events import SlotSet
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 #
 #
-class ActionHelloWorld(Action):
+class ActionTimeslot(Action):
 
     def name(self) -> Text:
         return "action_timeslot"
@@ -22,9 +22,23 @@ class ActionHelloWorld(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 	
-        timeslot = tracker.get_slot("timeslot")
+        timeslot = tracker.latest_message["text"]
         message = "Perfect. We have booked an appointment with Dr Tijmen at {}. We will be video-calling you through WhatsApp at this number then.".format(timeslot)
-        print(message)
+        
         dispatcher.utter_message(text=message)
 
+        return [SlotSet("timeslot", timeslot)]
+
+class ActionRepeat(Action):
+    
+    def name(self) -> Text:
+        return "action_repeat"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        name = tracker.get_slot("timeslot")
+        if not name:
+            dispatcher.utter_message(text="You haven't made an appointment yet.")
+        else:
+            dispatcher.utter_message(text=f"Your appointment is at {timeslot}!")
         return []
