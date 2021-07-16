7414fcdf9955d034c7b44520fb75afc7365f708b
tijmenvanetten <33256845+tijmenvanetten@users.noreply.github.com>
2021-07-16 17:00:13 +0200

    Finalize

--- external/AppointmentBot/actions/actions.py
+++ external/AppointmentBot/actions/actions.py
@@ -7,38 +7,35 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-from typing import Any, Text, Dict, List
-from rasa_sdk.events import SlotSet
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-#
-#
-class ActionTimeslot(Action):
+# from typing import Any, Text, Dict, List
+# from rasa_sdk.events import SlotSet
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+# #
+# #
+# class ActionTimeslot(Action):
 
-    def name(self) -> Text:
-        return "action_timeslot"
+#     def name(self) -> Text:
+#         return "action_timeslot"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 	
-        timeslot = tracker.latest_message["text"]
-        message = "Perfect. We have booked an appointment with Dr Tijmen at {}. We will be video-calling you through WhatsApp at this number then.".format(timeslot)
+#         message = f"Perfect. We have booked an appointment with a doctor. We will be video-calling you through WhatsApp at this number then."
         
-        dispatcher.utter_message(text=message)
+#         dispatcher.utter_message(reponse="utter_repeat", text=message)
 
-        return [SlotSet("timeslot", timeslot)]
+#         return []
 
-class ActionRepeat(Action):
+# class ActionRepeat(Action):
     
-    def name(self) -> Text:
-        return "action_repeat"
+#     def name(self) -> Text:
+#         return "action_repeat"
 
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        name = tracker.get_slot("timeslot")
-        if not name:
-            dispatcher.utter_message(text="You haven't made an appointment yet.")
-        else:
-            dispatcher.utter_message(text=f"Your appointment is at {timeslot}!")
-        return []
+#         tmessage = f"Perfect. We have booked an appointment with a doctor. We will be video-calling you through WhatsApp at this number then."
+        
+#         dispatcher.utter_message(reponse="utter_repeat", text=message)
+#         return []
--- external/run.py
+++ external/run.py
@@ -3,8 +3,10 @@ Instantiates all the bots listed in the external/ folder
 and enables their API on a unique port on the localhost
 """
 
-from pathlib import Path
+import os
+import signal
 import subprocess
+from pathlib import Path
 
 
 BASE_PORT = 5006
@@ -13,6 +15,7 @@ BASE_DIR = Path(__file__).parent
 # Iterate over folders in external/
 for child in BASE_DIR.glob('*/'):
     if child.is_dir():
+        print(f"Starting {child.name} server...")
         command = f'cd {child}/ & rasa run --model {child / "models/"} -p {BASE_PORT} --enable-api;'
-        subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+        pro = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
         BASE_PORT += 1
